dfd50601bc5096a7aec9dbd1d8a6d12e15415bea
thenetlog <emaildotmm@gmail.com>
2018-09-22 20:38:34 +0630

    Add errorMessage

--- Dtos/UserForRegisterDto.cs
+++ Dtos/UserForRegisterDto.cs
@@ -5,8 +5,9 @@ namespace DatingApp.API.Dtos
     // Data Transfer Objects
     public class UserForRegisterDto
     {
-        [Required]
+        [Required(ErrorMessage = "Username is required")]
         public string Username { get; set; }
+        
         [Required]
         [StringLength(8, MinimumLength = 4, ErrorMessage = "You must specify a password between 4 and 8 characters")]
         public string Password { get; set; }
